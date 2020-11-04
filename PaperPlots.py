b84d4b5c315685c50cd3c2f02750e110ae3c53e8
shm <u7028158@anu.edu.au>
2020-11-04 11:16:52 +1100

    All classes plot and modified galaxy clusters images plots

--- PaperPlots.py
+++ PaperPlots.py
@@ -116,7 +116,113 @@ def Combined_TPCF(save=False,outdir='../Results/',indir=None,method='masked_radi
 
 
     if(save):
-        filename = outdir+'Combined_TPCF_NoBounds.pdf'
+        filename = outdir+'Combined_TPCF_5kpcLimit.pdf'
+        plt.savefig(filename,bbox_inches='tight')
+        plt.close()
+    else :
+        plt.show()
+
+
+
+def Combined_TPCF_allclass(save=False,outdir='../Results/',indir=None,method='masked_radial'):
+    """
+    Plot the TPCF of all galaxies as a combined figure.
+
+    Parameters:
+        save: 
+            Flag to save the plot
+        outdir: 
+            Output directory in which to store plot. Default is results directory.
+        indir :
+            Input directory from which to read the results. Default is results directory.
+        method : 
+            Method for which TPCF's have been computed.
+    Returns:
+        None
+
+
+    """
+    
+    #Create figure and axs instance
+    fig,axs = plt.subplots(nrows=3,ncols=4,figsize=(16,12))
+
+    if(indir == None):
+        indir = os.path.abspath('../Results/Galaxies/')+'/'
+        method_dir = ''
+    else :
+        method_dir = None
+
+    #Directories
+    indir = os.path.abspath(indir)+'/'
+    outir = os.path.abspath(outdir)+'/'
+
+    if(method_dir is not None):
+        if(method.upper() == 'MASKED'):
+            method_dir = 'Masked/'
+        elif(method.upper() == 'UNIFORM'):
+            method_dir = 'Uniform/'
+        elif(method.upper() == 'MASKED_RADIAL'):
+            method_dir = 'Masked_Radial/'
+        else:
+            raise myError("Method not recognised.")
+
+
+    i,j = 0,0            
+    #Loop through the galaxies
+    for galaxy_name in list_of_galaxies:
+        if(method_dir == None):
+            galaxy_dir = indir+galaxy_name+'/'            
+        else :
+            galaxy_dir = indir+galaxy_name+'/'+method_dir
+
+        galaxy_class = loadObj(galaxy_dir+galaxy_name+'_summary')
+        plot_class = myPlot(galaxy_class)
+
+        #Secondary axis
+        separation_bins = galaxy_class.bin_centres*(1./arcsec_to_degree)
+        
+            
+        axs[i,j].set_yscale('log')
+        ax2 = axs[i,j].secondary_xaxis("top",functions=(plot_class.sep_to_pc,plot_class.pc_to_sep))
+        
+        color = ['#E13B58','#326EFA','#A5F597']
+        for k in range(1,4):                
+            galaxy_class.Compute_TPCF(cluster_class=k,random_method=method)                    
+            axs[i,j].errorbar(separation_bins,galaxy_class.corr,yerr=galaxy_class.dcorr,
+                fmt='.-',label='Class {}'.format(k),color=color[k-1])
+
+        #Plot stuff
+        #X-labels only on bottom row
+        if(i==2):
+            axs[i,j].set_xlabel(r"$\theta \, \left(\mathrm{arcsec} \right)$")
+        #Y-labels only on left column
+        if(j == 0):
+            axs[i,j].set_ylabel(r"$\omega_{\mathrm{LS}}\left(\theta \right)$")
+        axs[i,j].set_xscale('log')
+        axs[i,j].callbacks.connect("xlim_changed", plot_class.axs_to_parsec)
+        if(i+j == 0):
+            axs[i,j].legend()
+
+        #Secondary axis label only for top row
+        if(i==0):
+            ax2.set_xlabel(r'$\delta x \, \left( \mathrm{pc} \right) $')
+
+        
+        axs[i,j].text(0.1,0.1,r'$\mathrm{NGC}$'+' '+r'${}$'.format(galaxy_name.split('_')[1]),
+            transform=axs[i,j].transAxes)
+
+        
+        
+
+        #Get position of subplot
+        j +=1
+        if(j==4):
+            j = 0
+            i +=1
+
+
+    if(save):
+        filename = outdir+'Combined_TPCF_Classes.pdf'
         plt.savefig(filename,bbox_inches='tight')
         plt.close()
     else :
@@ -193,7 +299,7 @@ def Combined_RGBImages(save=False,outdir='../Results/',indir=None,method='masked
 
 def Combined_Clusters(save=False,outdir='../Results/',indir=None,method='masked_radial'):
     #Create figure and axs instance
-    fig = plt.figure(figsize=(16,20),constrained_layout=True)
+    fig = plt.figure(figsize=(20,16),constrained_layout=True)
 
     if(indir == None):
         indir = os.path.abspath('../Results/Galaxies/')+'/'
@@ -226,27 +332,72 @@ def Combined_Clusters(save=False,outdir='../Results/',indir=None,method='masked_
 
         galaxy_class = loadObj(galaxy_dir+galaxy_name+'_summary')
         plot_class = myPlot(galaxy_class)
+        ages = galaxy_class.get_cluster_ages()
+        cmap = cmr.waterlily
 
-        #WCS info
-        hdu = fits.open(galaxy_class.fits_file)[0]
-        wcs = WCS(hdu.header)        
+        ax1 = plt.subplot2grid((3,4),(i,j),fig=fig)
 
-        axs = plt.subplot2grid((4,3),(i,j),projection=wcs,fig=fig)
-        im = axs.scatter(galaxy_class.ra,galaxy_class.dec)
+        x = galaxy_class.ra*3600.0
+        y = galaxy_class.dec*3600.0
 
-        if(i==3):
-            axs.set_xlabel(r"$\mathrm{Right \; Ascension \; (J2000)}$",fontsize=20)
-        if(j==0):
-            axs.set_ylabel(r"$\mathrm{Declination \; (J2000)}$",labelpad=-0.8,
-                       fontsize=20)
+        #Get central pixel values
+        xcen = (np.min(x)+np.max(x))/2.
+        ycen = (np.min(y)+np.max(y))/2.
+
+        #Scale offset around bounding box to ~ 5% of axes width
+        offset_x = (np.max(x)-np.min(x))*0.05
+        offset_y = (np.max(y)-np.min(y))*0.05
+
+        xmin,xmax = np.min(x)-offset_x-xcen, np.max(x)+offset_x-xcen
+        ymin,ymax = np.min(y)-offset_y-ycen, np.max(y)+offset_y-ycen
+        ax1.set_xlim(xmin,xmax)
+        ax1.set_ylim(ymin,ymax)
+
+        im1 = ax1.scatter(x-xcen,y-ycen,c=np.log10(ages),alpha=0.5,cmap=cmap)
+        cbar = fig.colorbar(im1,ax = ax1,use_gridspec=False,
+                            orientation='vertical',pad=0.00,aspect=30)
         
-        axs.text(0.1,0.1,r'$\mathrm{NGC}$'+' '+r'${}$'.format(galaxy_name.split('_')[1]),
-            transform=axs.transAxes)
+
+        # #Draw 100 arcsec scale bar
+        import matplotlib.lines as lines
+        #No of pixels in axes
+        total_pixels = np.int(np.floor(ax1.transData.transform((xmax,ymax))[0]) - \
+        np.floor(ax1.transData.transform((xmin,ymin))[0]))
+
+        length_per_pixel = (xmax-xmin)/(total_pixels)
+        #Convert to parsec 
+        length_per_pixel = plot_class.sep_to_pc(length_per_pixel)
+        #Scale bar of 50 arcsec
+        length = plot_class.sep_to_pc(50)
+        no_pixels = np.abs(length/length_per_pixel)
+        no_pixels = no_pixels/total_pixels
+
+        scale = lines.Line2D([0.8,0.8+no_pixels],[0.1],
+                                         lw=1,color='black',
+                                        transform=ax1.transAxes)
+
+        ax1.add_line(scale)
+        ax1.annotate(r'$50^{\prime \prime} = %d \, \mathrm{pc}$'%length,(0.65,0.15),xycoords='axes fraction',
+                            fontsize=12)
+
+
+        if(i==2):
+            ax1.set_xlabel(r'$\mathrm{separation} \, (\mathrm{arcsec})$',fontsize=16)
+
+        if(j==0):
+            ax1.set_ylabel(r'$\mathrm{separation} \, (\mathrm{arcsec})$',fontsize=16)
+
+        if(j==3):    
+            cbar.ax.set_ylabel(r"$\log_{10} \, \mathrm{Age} \, (\mathrm{yr})$",
+                rotation=90,labelpad=5,fontsize=20)
+        
+        ax1.text(0.1,0.1,r'$\mathrm{NGC}$'+' '+r'${}$'.format(galaxy_name.split('_')[1]),
+            transform=ax1.transAxes)
 
         #Get position of subplot
         #Get position of subplot
         j +=1
-        if(j==3):
+        if(j==4):
             j = 0
             i +=1
 
@@ -350,4 +501,6 @@ def Scatter_Correlations(save=False,outdir='../Results/',indir=None,method='mask
 if __name__ == "__main__":
     print("Preparing plots of paper.")
     Combined_TPCF(save=True,method='masked')
+    # Combined_Clusters(save=True,method='masked')
+    # Scatter_Correlations(save=True,method='masked')
 
