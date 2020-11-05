941b246c12dd1dd50e5a7a2c08ebb3f8886399bc
shm <u7028158@anu.edu.au>
2020-11-06 08:17:26 +1100

    Added plot for combined TPCF only for young or old clusters

--- PaperPlots.py
+++ PaperPlots.py
@@ -116,11 +116,154 @@ def Combined_TPCF(save=False,outdir='../Results/',indir=None,method='masked_radi
 
 
     if(save):
-        filename = outdir+'Combined_TPCF_5kpcLimit.pdf'
+        filename = outdir+'Combined_TPCF_MaskedRadial.pdf'
         plt.savefig(filename,bbox_inches='tight')
         plt.close()
     else :
         plt.show()
+
+
+
+def Combined_TPCF_Ages(save=False,outdir='../Results/',indir=None,method='masked_radial',age_group='young'):
+    """
+    Plot the TPCF of clusters with age < 10 Myr in all galaxies as a combined figure.
+
+    Parameters:
+        save: 
+            Flag to save the plot
+        outdir: 
+            Output directory in which to store plot. Default is results directory.
+        indir :
+            Input directory from which to read the results. Default is results directory.
+        method : 
+            Method for which TPCF's have been computed.
+        age_group : 
+            Whether to do this for young or old clusters where young<10 Myr and old > 10 Myr    
+    Returns:
+        None
+
+
+    """
+    
+    #Create figure and axs instance
+    fig,axs = plt.subplots(nrows=3,ncols=4,figsize=(16,12))
+
+    if(indir == None):
+        indir = os.path.abspath('../Results/Galaxies/')+'/'
+        method_dir = ''
+    else :
+        method_dir = None
+
+    #Directories
+    indir = os.path.abspath(indir)+'/'
+    outir = os.path.abspath(outdir)+'/'
+
+    if(method_dir is not None):
+        if(method.upper() == 'MASKED'):
+            method_dir = 'Masked/'
+        elif(method.upper() == 'UNIFORM'):
+            method_dir = 'Uniform/'
+        elif(method.upper() == 'MASKED_RADIAL'):
+            method_dir = 'Masked_Radial/'
+        else:
+            raise myError("Method not recognised.")
+
+    if(age_group not in ['young','old']):
+        raise myError("Please specify either young or old in age group.")
+
+
+    i,j = 0,0            
+    #Loop through the galaxies
+    for galaxy_name in list_of_galaxies:
+        if(method_dir == None):
+            galaxy_dir = indir+galaxy_name+'/'            
+        else :
+            galaxy_dir = indir+galaxy_name+'/'+method_dir
+
+        galaxy_class = loadObj(galaxy_dir+galaxy_name+'_summary')
+        plot_class = myPlot(galaxy_class)
+
+        #Secondary axis
+        separation_bins = galaxy_class.bin_centres*(1./arcsec_to_degree)
+        
+        ages = galaxy_class.get_cluster_ages()
+        #Clusters < 10Myr
+
+        if(age_group == 'young'):
+            galaxy_class.ra = galaxy_class.ra[np.where(ages <= 1.e7)]
+            galaxy_class.dec = galaxy_class.dec[np.where(ages <= 1.e7)] 
+        elif(age_group == 'old'):
+            galaxy_class.ra = galaxy_class.ra[np.where(ages > 1.e7)]
+            galaxy_class.dec = galaxy_class.dec[np.where(ages > 1.e7)]
+
+        corr,dcorr,bootstraps = bootstrap_two_point_angular(galaxy_class,
+                        method='landy-szalay',Nbootstraps=100,
+                        random_method=method)
+        axs[i,j].errorbar(separation_bins,corr,yerr=dcorr,
+            fmt='.-')
+
+        galaxy_class.corr = corr 
+        galaxy_class.dcorr = dcorr
+
+        galaxy_class.fit_power_law(use_bounds=False)
+        
+        axs[i,j].set_yscale('log')            
+        ax2 = axs[i,j].secondary_xaxis("top",functions=(plot_class.sep_to_pc,plot_class.pc_to_sep))
+        
+        #Fit to TPCF
+
+
+        #Fit plot
+        break_theta = np.exp(galaxy_class.fit_values[3])
+        break_theta_error = np.exp(galaxy_class.fit_errors[3])
+        axs[i,j].plot(separation_bins,np.exp(linear_function(separation_bins,galaxy_class.fit_values[0],
+            galaxy_class.fit_values[1],galaxy_class.fit_values[2],galaxy_class.fit_values[3])),
+            ls='--',label='fit')
+        axs[i,j].plot(separation_bins,galaxy_class.corr,lw=0.0,
+            label=r'$\alpha_1 = {:2.1f} \pm {:2.1f}$'.format(galaxy_class.fit_values[1],galaxy_class.fit_errors[1]))
+        axs[i,j].plot(separation_bins,galaxy_class.corr,lw=0.0,
+            label=r'$\alpha_2 = {:2.1f} \pm {:2.1f}$'.format(galaxy_class.fit_values[2],galaxy_class.fit_errors[2]))
+        axs[i,j].axvline(break_theta,ls=':',label=r'$\beta = {:2.1f} \pm {:2.1f}$'.format(break_theta,
+            break_theta_error))
+
+        #Plot stuff
+        #X-labels only on bottom row
+        if(i==2):
+            axs[i,j].set_xlabel(r"$\theta \, \left(\mathrm{arcsec} \right)$")
+        #Y-labels only on left column
+        if(j == 0):
+            axs[i,j].set_ylabel(r"$\omega_{\mathrm{LS}}\left(\theta \right)$")
+        axs[i,j].set_xscale('log')
+        axs[i,j].callbacks.connect("xlim_changed", plot_class.axs_to_parsec)
+        axs[i,j].legend()
+
+        #Secondary axis label only for top row
+        if(i==0):
+            ax2.set_xlabel(r'$\delta x \, \left( \mathrm{pc} \right) $')
+
+        
+        axs[i,j].text(0.1,0.1,r'$\mathrm{NGC}$'+' '+r'${}$'.format(galaxy_name.split('_')[1]),
+            transform=axs[i,j].transAxes)
+
+        
+        
+
+        #Get position of subplot
+        j +=1
+        if(j==4):
+            j = 0
+            i +=1
+
+
+    if(save):
+        if(age_group == 'young'):
+            filename = outdir+'Combined_TPCF_Young.pdf'
+        elif(age_group == 'old'):
+            filename = outdir+'Combined_TPCF_Old.pdf'    
+        plt.savefig(filename,bbox_inches='tight')
+        plt.close()
+    else :
+        plt.show()        
 
 
 
@@ -500,7 +643,10 @@ def Scatter_Correlations(save=False,outdir='../Results/',indir=None,method='mask
 
 if __name__ == "__main__":
     print("Preparing plots of paper.")
-    Combined_TPCF(save=True,method='masked')
-    # Combined_Clusters(save=True,method='masked')
+    Combined_TPCF(save=True,method='masked_radial')
+    #Combined_TPCF_allclass(save=True,method='masked')
+    #Combined_TPCF_Ages(save=True,method='masked',age_group='young')
+    #Combined_TPCF_Ages(save=True,method='masked',age_group='old')
+    #Combined_Clusters(save=True,method='masked')
     # Scatter_Correlations(save=True,method='masked')
 
