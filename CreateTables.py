c255c5c4818224157ad5e0b9613d6bfa0f8e80b1
shm <u7028158@anu.edu.au>
2020-11-17 19:50:22 +1100

    Added table that compares AIC values and fits

--- CreateTables.py
+++ CreateTables.py
@@ -114,9 +114,122 @@ def GalaxyInfo_Table():
     return
 
 
+def AIC_Table():
+    print("Making AIC summary table.")
+    print("####################################")
+    results_directory = '../Results/'
+    filename = results_directory+'AIC.tex'
+
+    with open(filename,"w") as file :
+        #Header and table columns
+        print("\\documentclass[a4paper]{article}",file=file)
+        print("\\usepackage{booktabs}",file=file)
+        print("\\usepackage{fouriernc}",file=file)
+        print("\\usepackage[flushleft]{threeparttable}",file=file)
+        print("\\renewcommand{\\arraystretch}{1.7}",file=file)
+        print("",file=file)
+        print("\\begin{document}",file=file)
+        print("",file=file)
+        print("",file=file)
+        print("\\begin{table*}",file=file)
+        print("\\centering",file=file)
+        print("\\label{tab:galaxyinfo}",file=file)
+        print("\\begin{threeparttable}",file=file)
+        print("\\caption{MCMC best fit parameters and associated Akaike Information Criterion (AIC) values.}",file=file) 
+        print("\\begin{tabular}{l c c c c c c c}",file=file)
+        print("\\toprule",file=file)    
+        print("\\multicolumn{1}{l}{Galaxy}& \\multicolumn{3}{c}{Piecewise} & \\multicolumn{1}{c}{$\\mathrm{AIC}_{\\mathrm{PW}}$} &\
+\\multicolumn{1}{c}{Single} & \\multicolumn{1}{c}{$\\mathrm{AIC}_{\\mathrm{S}}$} & \\multicolumn{1}{c}{Model} \\\\ % Column names row",file=file)
+        print(" &$\\alpha_1$&$\\alpha_2$&$\\beta$ & & $\\alpha$ & & \\\\",file=file)
+        print("\\midrule",file=file)
+
+        i = 0
+
+        for galaxy_name in list_of_galaxies:
+            galaxy_dir = "../Results/Galaxies/{}/Masked".format(galaxy_name)
+            galaxy_class = loadObj("{}/{}_summary".format(galaxy_dir,galaxy_name))
+
+            #Read in PW fits
+            sampler = loadObj(galaxy_dir+'/PiecePL_MCMC/'+'MCMC_sampler')
+            samples = sampler.flatchain
+
+            PW_median_fit = np.percentile(samples,50,axis=0)
+            PW_error_low = np.percentile(samples,16,axis=0)
+            PW_error_low = PW_median_fit-PW_error_low
+            PW_error_high = np.percentile(samples,84,axis=0)
+            PW_error_high = PW_error_high - PW_median_fit
+
+            #Account for fact result is log(beta)
+            PW_error_low[3] = np.exp(PW_error_low[3])
+            PW_error_high[3] = np.exp(PW_error_high[3])
+            PW_median_fit[3] = np.exp(PW_median_fit[3])
+
+            #Read in Single PL fit
+            sampler = loadObj(galaxy_dir+'/SinglePL_MCMC/'+'MCMC_sampler')
+            samples = sampler.flatchain
+
+            S_median_fit = np.percentile(samples,50,axis=0)
+            S_error_low = np.percentile(samples,16,axis=0)
+            S_error_low = S_median_fit-S_error_low
+            S_error_high = np.percentile(samples,84,axis=0)
+            S_error_high = S_error_high - S_median_fit
+            
+            #Get AIC values
+            AIC_S, AIC_PW = compare_AIC(galaxy_name)
+
+            if(AIC_S<AIC_PW):
+                model = 'S'
+            else:
+                model = 'PW'
+
+            #Write to table
+            row_str = ""
+            name = galaxy_class.name.split('_')[0] + ' ' + galaxy_class.name.split('_')[1]
+            row_str +="{} &".format(name)
+
+            for j in range(1,4):
+                row_str +="${:2.1f}^{{+{:3.2f}}}_{{-{:3.2f}}} $&".format(PW_median_fit[j],PW_error_high[j],PW_error_low[j])
+
+            row_str +="${:2.1f} $&".format(AIC_PW)
+
+            row_str +="${:2.1f}^{{+{:3.2f}}}_{{-{:3.2f}}} $&".format(S_median_fit[1],S_error_high[1],S_error_high[1])
+
+            row_str +="${:2.1f} $ &".format(AIC_S)
+            row_str +="{}".format(model)
+            row_str +="\\\\"
+
+            print(row_str,file=file)
+        
+        print("\\bottomrule",file=file)
+        print("\\end{tabular}",file=file)
+        print("\\end{threeparttable}",file=file)
+        print("\\end{table*}",file=file)
+        print("\\end{document}",file=file)
+
+        print("Printed table to {}".format(filename))
+        print("Creating PDF with pdflatex")
+        subprocess.run(["pdflatex","-output-directory={}".format(results_directory),"{}".format(filename)], stdout=subprocess.DEVNULL)
+        filename_pdf = os.path.splitext(filename)[0]+'.pdf'
+        subprocess.run(["pdfcrop","{}".format(filename_pdf),"{}".format(filename_pdf)], stdout=subprocess.DEVNULL)
+        subprocess.run(["rm","{}Summary_Galaxy.aux".format(results_directory)], stdout=subprocess.DEVNULL)
+        subprocess.run(["rm","{}Summary_Galaxy.log".format(results_directory)], stdout=subprocess.DEVNULL)
+        subprocess.run(["rm","-rf","{}log/".format(results_directory)], stdout=subprocess.DEVNULL)
+        return
 
 def GalaxyTPCF_Table():
 
     return
 
+def compare_AIC(galaxy_name):
+    #Read in samplers
+    
+    sampler_single = loadObj('../Results/Galaxies/{}/Masked/\
+SinglePL_MCMC/MCMC_sampler'.format(galaxy_name))
 
+    sampler_piecewise = loadObj('../Results/Galaxies/{}/Masked/\
+PiecePL_MCMC/MCMC_sampler'.format(galaxy_name))
+    
+    AIC_single = 2*2.0 - np.max(sampler_single.flatlnprobability)
+    AIC_piecewise = 2*4.0 - np.max(sampler_piecewise.flatlnprobability)
+    
+    return AIC_single,AIC_piecewise
