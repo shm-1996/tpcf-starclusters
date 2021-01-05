7a5b213c763bc3f3382d2b40b27e8c7a4073d3ab
shm <u7028158@anu.edu.au>
2021-01-05 16:40:08 +1100

    Added SF regions with Poisson

--- ToyModels.py
+++ ToyModels.py
@@ -575,6 +575,93 @@ def Analyse_SF(r_SF,n_regions=30,n_points=100,compute_TPCF=False,
     else:
         plt.show()
 
+def Create_Poisson(lambda_poisson=10000,xMin=0.0,xMax=1.0,yMin=0.0,yMax=1.0):
+    #Simulation window parameters
+    xDelta=xMax-xMin;yDelta=yMax-yMin; #rectangle dimensions
+    areaTotal=xDelta*yDelta;
+
+    #Point process parameters
+    lambda0=lambda_poisson; #intensity (ie mean density) of the Poisson process
+
+    #Simulate Poisson point process
+    numbPoints = scipy.stats.poisson( lambda0*areaTotal ).rvs()#Poisson number of points
+    xr = xDelta*scipy.stats.uniform.rvs(0,1,((numbPoints,1)))+xMin#x coordinates of Poisson points
+    yr = yDelta*scipy.stats.uniform.rvs(0,1,((numbPoints,1)))+yMin#y coordinates of Poisson points
+    xr = xr.flatten()
+    yr = yr.flatten()
+    return xr,yr
+    
+
+def Create_SF_Poisson(r_SF,poisson_fraction=1.0,n_regions=100,
+                      n_points=100):
+    n_sf = n_regions*n_points
+    n_poisson = poisson_fraction*n_sf
+    xr_sf, yr_sf = Create_SF(r_SF,n_regions,n_points)
+    xr_poisson,yr_poisson = Create_Poisson(lambda_poisson=n_poisson)
+    xr,yr = np.append(xr_sf,xr_poisson),np.append(yr_sf,yr_poisson)
+    return xr,yr
+
+
+def Analyse_SF_Poisson(r_SF,poisson_fraction=1.0,
+                       n_regions=30,n_points=100,compute_TPCF=False,
+                      save=False):
+    filename = '../Toy_Models/SF_Poisson_Regions/SF_%02d'%(r_SF*100)
+    xr,yr = Create_SF_Poisson(r_SF,poisson_fraction,n_regions,n_points)
+    bins = np.logspace(np.log10(r_SF/100.),np.log10(r_SF*4.0),30)
+    
+    if(compute_TPCF is True):
+        print("Computing TPCF")
+        corr_lz,dcorr_lz = SF_TPCF(xr,yr,bins)
+        saveObj(corr_lz,'{}_corr'.format(filename))
+        saveObj(dcorr_lz,'{}_dcorr'.format(filename))
+    else:
+        print("Reading")
+        corr_lz = loadObj('{}_corr'.format(filename))
+        dcorr_lz = loadObj('{}_dcorr'.format(filename))
+        
+    print("Fitting")
+    #Fit line to this
+    from TPCF import linear_truncation
+    separation_bins = (bins[1:]+bins[:-1])/2.
+    indices = np.where(corr_lz>0.0)
+    dcorr_lz = dcorr_lz[indices]
+    separation_bins = separation_bins[indices]
+    corr_lz = corr_lz[indices]
+    
+    bounds = ([-10.0,-3.0,np.min(separation_bins)],[10.0,0.0,np.max(separation_bins)*3.0])
+    popt,pcov = curve_fit(linear_truncation,separation_bins,
+                        np.log(corr_lz),sigma=dcorr_lz/corr_lz,bounds=bounds)
+    perr= np.sqrt(np.diag(pcov))
+    
+    print("Plotting")
+    fig,axs = plt.subplots(nrows=2,figsize=(4,8))
+
+    axs[0].scatter(xr,yr,s=0.4,alpha=0.2,color='red')
+
+
+    #Landy-Szalay version
+    
+    axs[1].errorbar(separation_bins,corr_lz,yerr=dcorr_lz,
+                 fmt='.-')
+    axs[1].axvline(r_SF,ls=':',label=r'$r_\mathrm{SF}$'+r'$= {}$'.format(r_SF))
+
+    axs[1].plot(separation_bins,np.exp(linear_truncation(separation_bins,
+                                                        popt[0],popt[1],popt[2])),
+            ls='--',label=r'$\alpha = {:3.2f} \pm {:3.2f}$'.format(popt[1],perr[1]))
+
+    axs[1].axvline(popt[2],ls='--',color='#F4271C',
+                  label=r'$\theta_c = {:3.2f} \pm {:3.2f}$'.format(popt[2],perr[2]))
+
+    axs[1].set_xlabel(r"$\Delta x \, (\mathrm{pixels})$")
+    axs[1].set_ylabel(r"$\omega_{\mathrm{LS}}\left(\theta \right)$")
+    axs[1].legend(loc='best')
+    axs[1].set_xscale('log')
+    axs[1].set_yscale('log')
+    if(save):
+        plt.savefig('{}'.format(filename),bbox_inches='tight')
+    else:
+        plt.show()
+
 if __name__ == "__main__":
     Pitch = 40.0
     Analyse_Spiral(0.1,Pitch = Pitch,compute_TPCF=True,save=True)
