c9df8aa659ec68136efc2228f07c30e5f7ff9ab8
shm <u7028158@anu.edu.au>
2021-02-15 20:25:57 +1100

    Plot 1+omega now, and use Fractal gadi TPCF functions

--- ToyModels.py
+++ ToyModels.py
@@ -10,6 +10,7 @@ from sklearn.neighbors import BallTree
 from astroML.utils import check_random_state
 from TPCF import linear_function,onepowerlaw_function,linear_truncation
 from matplotlib.transforms import Bbox
+from Fractal_gadi import * 
 
 # Check if scikit-learn's two-point functionality is available.
 # This was added in scikit-learn version 0.14
@@ -22,375 +23,6 @@ except ImportError:
 
 #TPCF Functions
 
-def uniform_sphere(RAlim, DEClim, size=1):
-    """Draw a uniform sample on a sphere
-
-    Parameters
-    ----------
-    RAlim : tuple
-        select Right Ascension between RAlim[0] and RAlim[1]
-        units are degrees
-    DEClim : tuple
-        select Declination between DEClim[0] and DEClim[1]
-    size : int (optional)
-        the size of the random arrays to return (default = 1)
-
-    Returns
-    -------
-    RA, DEC : ndarray
-        the random sample on the sphere within the given limits.
-        arrays have shape equal to size.
-    """
-    zlim = np.sin(np.pi * np.asarray(DEClim) / 180.)
-
-    z = zlim[0] + (zlim[1] - zlim[0]) * np.random.random(size)
-    DEC = (180. / np.pi) * np.arcsin(z)
-    RA = RAlim[0] + (RAlim[1] - RAlim[0]) * np.random.random(size)
-
-    return RA, DEC
-
-
-def ra_dec_to_xyz(ra, dec):
-    """Convert ra & dec to Euclidean points
-
-    Parameters
-    ----------
-    ra, dec : ndarrays
-
-    Returns
-    x, y, z : ndarrays
-    """
-    sin_ra = np.sin(ra * np.pi / 180.)
-    cos_ra = np.cos(ra * np.pi / 180.)
-
-    sin_dec = np.sin(np.pi / 2 - dec * np.pi / 180.)
-    cos_dec = np.cos(np.pi / 2 - dec * np.pi / 180.)
-
-    return (cos_ra * sin_dec,
-            sin_ra * sin_dec,
-            cos_dec)
-
-
-def angular_dist_to_euclidean_dist(D, r=1):
-    """convert angular distances to euclidean distances"""
-    return 2 * r * np.sin(0.5 * D * np.pi / 180.)
-
-
-def two_point(data, bins, method='standard',
-              data_R=None, random_state=None):
-    """Two-point correlation function
-
-    Parameters
-    ----------
-    data : array_like
-        input data, shape = [n_samples, n_features]
-    bins : array_like
-        bins within which to compute the 2-point correlation.
-        shape = Nbins + 1
-    method : string
-        "standard" or "landy-szalay".
-    data_R : array_like (optional)
-        if specified, use this as the random comparison sample
-    random_state : integer, np.random.RandomState, or None
-        specify the random state to use for generating background
-
-    Returns
-    -------
-    corr : ndarray
-        the estimate of the correlation function within each bin
-        shape = Nbins
-    """
-    data = np.asarray(data)
-    bins = np.asarray(bins)
-    rng = check_random_state(random_state)
-
-    if method not in ['standard', 'landy-szalay']:
-        raise ValueError("method must be 'standard' or 'landy-szalay'")
-
-    if bins.ndim != 1:
-        raise ValueError("bins must be a 1D array")
-
-    if data.ndim == 1:
-        data = data[:, np.newaxis]
-    elif data.ndim != 2:
-        raise ValueError("data should be 1D or 2D")
-
-    n_samples, n_features = data.shape
-    Nbins = len(bins) - 1
-
-    # shuffle all but one axis to get background distribution
-    if data_R is None:
-        data_R = data.copy()
-        for i in range(n_features - 1):
-            rng.shuffle(data_R[:, i])
-    else:
-        data_R = np.asarray(data_R)
-        if (data_R.ndim != 2) or (data_R.shape[-1] != n_features):
-            raise ValueError('data_R must have same n_features as data')
-
-    factor = len(data_R) * 1. / len(data)
-
-    if sklearn_has_two_point:
-        # Fast two-point correlation functions added in scikit-learn v. 0.14
-        KDT_D = KDTree(data)
-        KDT_R = KDTree(data_R)
-
-        counts_DD = KDT_D.two_point_correlation(data, bins)
-        counts_RR = KDT_R.two_point_correlation(data_R, bins)
-
-    else:
-        warnings.warn("Version 0.3 of astroML will require scikit-learn "
-                      "version 0.14 or higher for correlation function "
-                      "calculations. Upgrade to sklearn 0.14+ now for much "
-                      "faster correlation function calculations.")
-
-        BT_D = BallTree(data)
-        BT_R = BallTree(data_R)
-
-        counts_DD = np.zeros(Nbins + 1)
-        counts_RR = np.zeros(Nbins + 1)
-
-        for i in range(Nbins + 1):
-            counts_DD[i] = np.sum(BT_D.query_radius(data, bins[i],
-                                                    count_only=True))
-            counts_RR[i] = np.sum(BT_R.query_radius(data_R, bins[i],
-                                                    count_only=True))
-
-    DD = np.diff(counts_DD)
-    RR = np.diff(counts_RR)
-
-    # check for zero in the denominator
-    RR_zero = (RR == 0)
-    RR[RR_zero] = 1
-
-    if method == 'standard':
-        corr = factor ** 2 * DD / RR - 1
-    elif method == 'landy-szalay':
-        if sklearn_has_two_point:
-            counts_DR = KDT_R.two_point_correlation(data, bins)
-        else:
-            counts_DR = np.zeros(Nbins + 1)
-            for i in range(Nbins + 1):
-                counts_DR[i] = np.sum(BT_R.query_radius(data, bins[i],
-                                                        count_only=True))
-        DR = np.diff(counts_DR)
-
-        corr = (factor ** 2 * DD - 2 * factor * DR + RR) / RR
-
-    corr[RR_zero] = np.nan
-
-    return corr
-
-
-
-def bootstrap_two_point(data, bins, Nbootstrap=10,
-                        method='standard', return_bootstraps=False,
-                        random_state=None):
-    """Bootstrapped two-point correlation function
-
-    Parameters
-    ----------
-    data : array_like
-        input data, shape = [n_samples, n_features]
-    bins : array_like
-        bins within which to compute the 2-point correlation.
-        shape = Nbins + 1
-    Nbootstrap : integer
-        number of bootstrap resamples to perform (default = 10)
-    method : string
-        "standard" or "landy-szalay".
-    return_bootstraps: bool
-        if True, return full bootstrapped samples
-    random_state : integer, np.random.RandomState, or None
-        specify the random state to use for generating background
-
-    Returns
-    -------
-    corr, corr_err : ndarrays
-        the estimate of the correlation function and the bootstrap
-        error within each bin. shape = Nbins
-    """
-    data = np.asarray(data)
-    bins = np.asarray(bins)
-    rng = check_random_state(random_state)
-
-    if method not in ['standard', 'landy-szalay']:
-        raise ValueError("method must be 'standard' or 'landy-szalay'")
-
-    if bins.ndim != 1:
-        raise ValueError("bins must be a 1D array")
-
-    if data.ndim == 1:
-        data = data[:, np.newaxis]
-    elif data.ndim != 2:
-        raise ValueError("data should be 1D or 2D")
-
-    if Nbootstrap < 2:
-        raise ValueError("Nbootstrap must be greater than 1")
-
-    n_samples, n_features = data.shape
-
-    # get the baseline estimate
-    corr = two_point(data, bins, method=method, random_state=rng)
-
-    bootstraps = np.zeros((Nbootstrap, len(corr)))
-
-    for i in range(Nbootstrap):
-        indices = rng.randint(0, n_samples, n_samples)
-        bootstraps[i] = two_point(data[indices, :], bins, method=method,
-                                  random_state=rng)
-
-    # use masked std dev in case of NaNs
-    corr_err = np.asarray(np.ma.masked_invalid(bootstraps).std(0, ddof=1))
-
-    if return_bootstraps:
-        return corr, corr_err, bootstraps
-    else:
-        return corr, corr_err
-
-
-
-def two_point_angular(ra, dec, bins, method='standard', random_state=None):
-    """Angular two-point correlation function
-
-    A separate function is needed because angular distances are not
-    euclidean, and random sampling needs to take into account the
-    spherical volume element.
-
-    Parameters
-    ----------
-    ra : array_like
-        input right ascention, shape = (n_samples,)
-    dec : array_like
-        input declination
-    bins : array_like
-        bins within which to compute the 2-point correlation.
-        shape = Nbins + 1
-    method : string
-        "standard" or "landy-szalay".
-    random_state : integer, np.random.RandomState, or None
-        specify the random state to use for generating background
-
-    Returns
-    -------
-    corr : ndarray
-        the estimate of the correlation function within each bin
-        shape = Nbins
-    """
-    ra = np.asarray(ra)
-    dec = np.asarray(dec)
-    rng = check_random_state(random_state)
-
-    if method not in ['standard', 'landy-szalay']:
-        raise ValueError("method must be 'standard' or 'landy-szalay'")
-
-    if bins.ndim != 1:
-        raise ValueError("bins must be a 1D array")
-
-    if (ra.ndim != 1) or (dec.ndim != 1) or (ra.shape != dec.shape):
-        raise ValueError('ra and dec must be 1-dimensional '
-                         'arrays of the same length')
-
-    n_features = len(ra)
-    Nbins = len(bins) - 1
-
-    # draw a random sample with N points
-    ra_R, dec_R = uniform_sphere((min(ra), max(ra)),
-                                 (min(dec), max(dec)),
-                                 2 * len(ra))
-
-    data = np.asarray(ra_dec_to_xyz(ra, dec), order='F').T
-    data_R = np.asarray(ra_dec_to_xyz(ra_R, dec_R), order='F').T
-
-    # convert spherical bins to cartesian bins
-    bins_transform = angular_dist_to_euclidean_dist(bins)
-
-    return two_point(data, bins_transform, method=method,
-                     data_R=data_R, random_state=rng)
-
-
-
-def bootstrap_two_point_angular(ra, dec, bins, method='standard',
-                                Nbootstraps=10, random_state=None):
-    """Angular two-point correlation function
-
-    A separate function is needed because angular distances are not
-    euclidean, and random sampling needs to take into account the
-    spherical volume element.
-
-    Parameters
-    ----------
-    ra : array_like
-        input right ascention, shape = (n_samples,)
-    dec : array_like
-        input declination
-    bins : array_like
-        bins within which to compute the 2-point correlation.
-        shape = Nbins + 1
-    method : string
-        "standard" or "landy-szalay".
-    Nbootstraps : int
-        number of bootstrap resamples
-    random_state : integer, np.random.RandomState, or None
-        specify the random state to use for generating background
-
-    Returns
-    -------
-    corr : ndarray
-        the estimate of the correlation function within each bin
-        shape = Nbins
-    dcorr : ndarray
-        error estimate on dcorr (sample standard deviation of
-        bootstrap resamples)
-    bootstraps : ndarray
-        The full sample of bootstraps used to compute corr and dcorr
-    """
-    ra = np.asarray(ra)
-    dec = np.asarray(dec)
-    rng = check_random_state(random_state)
-
-    if method not in ['standard', 'landy-szalay']:
-        raise ValueError("method must be 'standard' or 'landy-szalay'")
-
-    if bins.ndim != 1:
-        raise ValueError("bins must be a 1D array")
-
-    if (ra.ndim != 1) or (dec.ndim != 1) or (ra.shape != dec.shape):
-        raise ValueError('ra and dec must be 1-dimensional '
-                         'arrays of the same length')
-
-    n_features = len(ra)
-    Nbins = len(bins) - 1
-    data = np.asarray(ra_dec_to_xyz(ra, dec), order='F').T
-
-    # convert spherical bins to cartesian bins
-    bins_transform = angular_dist_to_euclidean_dist(bins)
-
-    bootstraps = []
-
-    for i in range(Nbootstraps):
-        # draw a random sample with N points
-        ra_R, dec_R = uniform_sphere((min(ra), max(ra)),
-                                     (min(dec), max(dec)),
-                                     2 * len(ra))
-
-        data_R = np.asarray(ra_dec_to_xyz(ra_R, dec_R), order='F').T
-
-        if i > 0:
-            # random sample of the data
-            ind = np.random.randint(0, data.shape[0], data.shape[0])
-            data_b = data[ind]
-        else:
-            data_b = data
-
-        bootstraps.append(two_point(data_b, bins_transform, method=method,
-                                    data_R=data_R, random_state=rng))
-
-    bootstraps = np.asarray(bootstraps)
-    corr = np.mean(bootstraps, 0)
-    corr_err = np.std(bootstraps, 0, ddof=1)
-
-    return corr, corr_err, bootstraps
 
 #############################################################################################
 # Toy Model 1 : Spiral Arms
@@ -447,7 +79,7 @@ def Analyse_Spiral(thickness,Pitch=10,compute_TPCF=False,save=False):
     
     bounds = ([-10.0,-3.0,np.min(separation_bins)],[10.0,0.0,np.max(separation_bins)*3.0])
     popt,pcov = curve_fit(linear_truncation,separation_bins,
-                        np.log(corr_lz),sigma=dcorr_lz/corr_lz,bounds=bounds)
+                        np.log(1+corr_lz),sigma=dcorr_lz/corr_lz,bounds=bounds)
     perr= np.sqrt(np.diag(pcov))
     
     print("Plotting")
@@ -458,7 +90,7 @@ def Analyse_Spiral(thickness,Pitch=10,compute_TPCF=False,save=False):
 
     #Landy-Szalay version
     
-    axs[1].errorbar(separation_bins,corr_lz,yerr=dcorr_lz,
+    axs[1].errorbar(separation_bins,1+corr_lz,yerr=dcorr_lz,
                  fmt='.-')
     axs[1].axvline(thickness,ls=':',label=r'$r_\mathrm{thick}$'+r'$= {}$'.format(thickness))
 
@@ -524,9 +156,9 @@ def SF_TPCF(xr,yr,bins):
 
 def Analyse_SF(r_SF,n_regions=30,n_points=100,compute_TPCF=False,
               save=False):
-    filename = '../Toy_Models/SF_Regions/SF_%02d'%(r_SF*100)
+    filename = '../Toy_Models/SF_Regions/Omega1/SF_{}'.format(r_SF)
     xr,yr = Create_SF(r_SF,n_regions,n_points)
-    bins = np.logspace(np.log10(r_SF/100.),np.log10(r_SF*4.0),30)
+    bins = np.logspace(np.log10(r_SF/100.),np.log10(r_SF*5.0),40)
     
     if(compute_TPCF is True):
         print("Computing TPCF")
@@ -549,7 +181,7 @@ def Analyse_SF(r_SF,n_regions=30,n_points=100,compute_TPCF=False,
     
     bounds = ([-10.0,-3.0,np.min(separation_bins)],[10.0,0.0,np.max(separation_bins)*3.0])
     popt,pcov = curve_fit(linear_truncation,separation_bins,
-                        np.log(corr_lz),sigma=dcorr_lz/corr_lz,bounds=bounds)
+                    np.log(1+corr_lz),sigma=dcorr_lz/corr_lz,bounds=bounds)
     perr= np.sqrt(np.diag(pcov))
     
     print("Plotting")
@@ -560,8 +192,8 @@ def Analyse_SF(r_SF,n_regions=30,n_points=100,compute_TPCF=False,
 
     #Landy-Szalay version
     
-    axs[1].errorbar(separation_bins,corr_lz,yerr=dcorr_lz,
-                 fmt='.-')
+    axs[1].errorbar(separation_bins,1+corr_lz,yerr=dcorr_lz,
+                 fmt='.-',lw=0.2)
     axs[1].axvline(r_SF,ls=':',label=r'$r_\mathrm{SF}$'+r'$= {}$'.format(r_SF))
 
     axs[1].plot(separation_bins,np.exp(linear_truncation(separation_bins,
@@ -572,12 +204,13 @@ def Analyse_SF(r_SF,n_regions=30,n_points=100,compute_TPCF=False,
                   label=r'$\theta_c = {:3.2f} \pm {:3.2f}$'.format(popt[2],perr[2]))
 
     axs[1].set_xlabel(r"$\Delta x \, (\mathrm{pixels})$")
-    axs[1].set_ylabel(r"$\omega_{\mathrm{LS}}\left(\theta \right)$")
+    axs[1].set_ylabel(r"$1+\omega_{\mathrm{LS}}\left(\theta \right)$")
     axs[1].legend(loc='best')
     axs[1].set_xscale('log')
     axs[1].set_yscale('log')
     if(save):
-        plt.savefig('{}'.format(filename),bbox_inches='tight')
+        plt.savefig('{}.pdf'.format(filename),bbox_inches='tight')
+        plt.close(fig)
     else:
         plt.show()
 
